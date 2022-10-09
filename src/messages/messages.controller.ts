93098d4566eb6bb0403889a0478137fa94b04358
Joao Pedro Aquino <aqerhack@gmail.com>
2022-10-09 11:07:50 -0300

    g findallmsg endpoint

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,4 +1,23 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 
 @Controller('messages')
-export class MessagesController {}
+export class MessagesController {
+    @Get()
+    findAll(){
+        return [
+            {
+                id: 1,
+                text:  'Primeria mensagem'
+            },
+
+            {
+                id: 2,
+                text:  'Segunda mensagem'
+            },
+            {
+                id: 3,
+                text:  'Terceira mensagem'
+            }
+        ];
+    }
+}
